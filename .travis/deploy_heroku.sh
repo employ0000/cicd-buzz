1b35248ef996afe8fc6262ab936ca12adf96041e
root <root@centos.local>
2019-01-16 12:35:22 +0300

    Deploy1

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:rm web --app $HEROKU_APP_NAME
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
